e80e5ea61f3253f18b74994d8507403db40971c5
ravikumardhotre <dhotreravi17@gmail.com>
2022-04-10 15:54:30 +0530

    hii

--- backend/server/db/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-const DB = process.env.DATABASE
-mongoose.connect(DB, {useNewUrlParser: true})
-    .then(() => console.log('mongodb running on 27017'))
-    .catch(err => console.log(err))
